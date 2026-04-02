989b2fa73ee4d39aea74b80527743c9b6d700731
sjack1630 <sjack1@terpmail.umd.edu>
2026-04-02 13:14:47 -0400

    transfer

--- runner.c
+++ runner.c
@@ -21,8 +21,10 @@ typedef enum {
 // declare variables
 motion_t cur_motion = STOP;
 orbit_state_t orbit_state = ORBIT_NORMAL;
-uint8_t cur_distance = 0;
+uint8_t cur_distance = UINT8_MAX;
 uint8_t new_message = 0;
+uint8_t rx_kilo_id;
+uint8_t cur_target_kilo_id;
 distance_measurement_t dist;
 
 void orbit_normal();
@@ -38,7 +40,6 @@ void runner_loop(){
     // Update distance estimate with every message
     if (new_message) {
         new_message = 0;
-        cur_distance = estimate_distance(&dist);
     } else if (cur_distance == 0) // skip state machine if no distance measurement available
         return;
 
@@ -96,10 +97,17 @@ void orbit_tooclose() {
 }
 
 void runner_message_rx(message_t *m, distance_measurement_t *d){
+    rx_kilo_id = m->data[0];
     new_message = 1;
     dist = *d;
+    uint8_t dist_val = estimate_distance(&dist);
 
-    // TODO: Make sure to only look at the closest robot
+    // Determine closest robot = target orbiting robot
+    if (dist_val < cur_distance && rx_kilo_id != cur_target_kilo_id){
+        cur_target_kilo_id = rx_kilo_id;
+    } else if (rx_kilo_id == cur_target_kilo_id){
+        cur_distance = dist_val;
+    }
 }
 message_t *runner_message_tx(){
     
